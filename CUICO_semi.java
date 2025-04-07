3fc364e92b2e0c5bf769f7574c7294f6378f1704
Saiyorii <saicuico@gmail.com>
2025-04-07 12:58:14 +0800

    Add files via upload

--- CUICO_semi.java
+++ CUICO_semi.java
@@ -1,42 +1,32 @@
 import javax.swing.*;
 
-
 public class CUICO_semi
 {
-   public static void main(String[] args)
-   {
-      String input = JOptionPane.showInputDialog("Enter a value in inches:");
-      double inches = Double.parseDouble(input);
+    public static void main(String[] args)
+    {
+        String input = JOptionPane.showInputDialog("Enter a value in inches:");
+        double inches = Double.parseDouble(input);
         
-      InchConversion conversion = new InchConversion(inches);
+        double feet = InchConversion.convertToFeet(inches);
+        double yards = InchConversion.convertToYards(inches);
         
-      double feet = conversion.convertToFeet();
-      double yards = conversion.convertToYards();
+        String messageFeet = "Calculation: Feet = inches / 12" + "\nInches: " + inches + "\n\nSolution: " + inches + " / 12" + "\n\nFeet = " + feet;
+        String messageYards = "Calculation: Yards = inches / 36" + "\nInches: " + inches + "\n\nSolution: " + inches + " / 36" + "\n\nYards = " + yards;
         
-
-      String messageFeet = "Calculation: Feet = inches / 12" + "\nInches: " + inches + "\n\nSolution: " + inches + " / 12" + "\n\nFeet = " + feet;
-      String messageYards = "Calculation: Yards = inches / 36" + "\nInches: " + inches + "\n\nSolution: " + inches + " / 36" + "\n\nYards = " + yards;
-        
-      JOptionPane.showMessageDialog(null, messageFeet);
-      JOptionPane.showMessageDialog(null, messageYards);
-   }
+        JOptionPane.showMessageDialog(null, messageFeet);
+        JOptionPane.showMessageDialog(null, messageYards);
+    }
 }
 
 class InchConversion
 {
-    private double inches;
-    public InchConversion(double inches)
+    public static double convertToFeet(double inches)
     {
-      this.inches = inches;
+        return inches / 12; // 12 inches in a foot
     }
 
-    public double convertToFeet()
+    public static double convertToYards(double inches)
     {
-      return inches / 12; // 12 inches in a foot
-    }
-    public double convertToYards()
-    {
-      return inches / 36; // 36 inches in a yard (3 feet)
+        return inches / 36; // 36 inches in a yard (3 feet)
     }
 }
-
